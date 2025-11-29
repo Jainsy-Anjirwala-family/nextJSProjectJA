2b5fc2b1d14b898d229ff6b8d4041c53dcddf11d
Jainsy Anjirwala <j.acreatordesign@gmail.com>
2025-11-29 15:10:17 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: 'export',
-    distDir: 'out',
+    distDir: '/out',
   /* config options here */
   images: {
     remotePatterns: [], // if you need remote images
